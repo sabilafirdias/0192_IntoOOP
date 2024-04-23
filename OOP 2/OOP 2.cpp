0c117451dda41d1e4108915d185f3a956b5c8d62
sabilafiridas <sabilafirdias@gmail.com>
2024-04-23 10:39:00 +0700

    membuat class bangundatar dengan akses private dan public

---
+++ OOP 2/OOP 2.cpp
@@ -0,0 +1,18 @@
+// OOP 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class bangunDatar {
+    private:
+        float panjang, lebar;
+    public:
+        float luas;
+};
+
+int main()
+{
+    
+}
+
